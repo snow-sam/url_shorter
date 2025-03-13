fe2df1193139e89909f1d6d96f553e25e6d3d9c0
Samuel Souza <samuel_f2@icloud.com>
2025-03-13 18:02:38 -0300

    ref: remove obsolete js file

--- routeHandler.js
+++
@@ -1,8 +0,0 @@
-// Pegando a rota acessada
-const path = window.location.pathname;
-console.log(path)
-if (path !== "/index.html") {
-    // Definindo o novo domínio
-    const newDomain = "https://www.outro-site.com";
-    window.location.href = `${newDomain}${path}`;
-}
